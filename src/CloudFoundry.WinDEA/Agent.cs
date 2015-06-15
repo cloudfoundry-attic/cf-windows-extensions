c944f5589173becdf8f83dcd8fb475f414e51e67
Stefan Schneider <stefan.schneider@hp.com>
2015-06-15 17:15:31 +0300

    Prevent deadlock when checking app port
    If two threads reach the SendHeartbeat at the same time, a deadlock will occur. To fix this, call SendHeartbeat outside of the instance's write lock.

--- src/CloudFoundry.WinDEA/Agent.cs
+++ src/CloudFoundry.WinDEA/Agent.cs
@@ -1879,7 +1879,6 @@
                                         instance.Properties.State = DropletInstanceState.Running;
                                         instance.Properties.StateTimestamp = DateTime.Now;
 
-                                        this.SendHeartbeat();
                                         this.RegisterInstanceWithRouter(instance);
                                         this.droplets.ScheduleSnapshotAppState();
                                     }
@@ -1893,6 +1892,11 @@
                             finally
                             {
                                 instance.Lock.ExitWriteLock();
+                            }
+
+                            if (detected)
+                            {
+                                this.SendHeartbeat();
                             }
                         });
                 });
